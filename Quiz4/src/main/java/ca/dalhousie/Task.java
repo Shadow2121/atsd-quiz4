521817649b174afaa57a3bc76d637ea0fdfae0e8
mdp <mh375200@dal.ca>
2025-03-05 15:21:54 -0400

    impliment actual assignTo().

--- Quiz4/src/main/java/ca/dalhousie/Task.java
+++ Quiz4/src/main/java/ca/dalhousie/Task.java
@@ -19,6 +19,10 @@ public class Task {
     }
 
     public boolean assignTo(TeamMember teamMember) {
+        if (teamMember == null) {
+            return false;
+        }
+        this.assignee = teamMember;
         return true;
     }
 }
